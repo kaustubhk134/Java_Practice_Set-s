8790c72e6929daeada79cca4f50d7f294cb76707
Kaustubh Kulurkar <154357336+kaustubhk134@users.noreply.github.com>
2024-06-15 16:29:24 +0530

    Avg Sum's

---
+++ Sheet 1 (Variables and Data Types)/P1.java
@@ -0,0 +1,18 @@
+// In a program,input 3 numbers: A, B and C .You have to output the average of these 3 numbers.(Hint : Average of N numbers is sum of those numbers divided by N)
+import java.util.*;
+public class P1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter FIRST no : ");
+        int a = sc.nextInt();
+        System.out.println("Enter SECOND no : ");
+        int b = sc.nextInt();
+        System.out.println("Enter THIRD no : ");
+        int c = sc.nextInt();
+
+        System.out.println("The Average is : "+ (a + b + c) / 3 );
+
+    }
+    
+}
