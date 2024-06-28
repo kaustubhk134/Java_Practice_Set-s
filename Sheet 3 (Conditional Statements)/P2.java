8d6ca8d7bcd965f18e2352132eeb2d7a36770c11
Kaustubh Kulurkar <154357336+kaustubhk134@users.noreply.github.com>
2024-06-29 00:23:45 +0530

    temperature if-else

---
+++ Sheet 3 (Conditional Statements)/P2.java
@@ -0,0 +1,16 @@
+// Finish the following code so that it prints You have a fever if your temperature is above 100 and otherwise prints You don't have a fever.
+import java.util.*;
+public class P2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.println("Enter your TEMPRATURE : ");
+        float temp = sc.nextFloat();
+        if (temp > 100){
+            System.out.println("You Have Fever");
+        }else{
+            System.out.println("You Don't have Fever");
+        }
+    }
+    
+}
