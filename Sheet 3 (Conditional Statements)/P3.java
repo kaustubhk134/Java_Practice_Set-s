1e323d43d045cc1563894dd3972d8128b4f29a7b
Kaustubh Kulurkar <154357336+kaustubhk134@users.noreply.github.com>
2024-07-01 15:58:52 +0530

    case statement

---
+++ Sheet 3 (Conditional Statements)/P3.java
@@ -0,0 +1,29 @@
+// Write a Java program to input week number(1-7) and print day of week name using switch case.
+import java.util.*;
+public class P3 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter the Number : ");
+        int num = sc.nextInt();
+
+        switch (num){
+            case 1 : System.out.println("MONDAY");
+                     break;
+            case 2 : System.out.println("TUESDAY");
+                     break;
+            case 3 : System.out.println("WEDNESDAY");
+                     break;
+            case 4 : System.out.println("THURSDAY");
+                     break;
+            case 5 : System.out.println("FRIDAY");
+                     break;
+            case 6 : System.out.println("SATURDAY");
+                     break;
+            case 7 : System.out.println("SUNDAY");
+                     break;
+            default : System.out.println("INVALID ENTRY");
+                    
+        }
+    }
+}
